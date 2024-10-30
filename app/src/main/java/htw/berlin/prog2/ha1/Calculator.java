a7a74f8dd2187348137f5abc44b89f87c1c82906
MahmudiIsnije <isnije01@hotmail.de>
2024-10-30 18:40:20 +0100

    3. Commit für den Fix zum ersten roten Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -118,16 +118,23 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
-            default -> throw new IllegalArgumentException();
-        };
-        screen = Double.toString(result);
-        if(screen.equals("Infinity")) screen = "Error";
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if (!latestOperation.isEmpty()) {
+            var currentScreenValue = Double.parseDouble(screen);
+            var result = switch (latestOperation) {
+                case "+" -> latestValue + currentScreenValue;
+                case "-" -> latestValue - currentScreenValue;
+                case "x" -> latestValue * currentScreenValue;
+                case "/" -> latestValue / currentScreenValue;
+                default -> throw new IllegalArgumentException();
+            };
+            screen = Double.toString(result);
+            latestValue = currentScreenValue; // speichert aktuellen Bildschirmwert
+            if (screen.equals("Infinity")) screen = "Error";
+            if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+            if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        }
     }
+
 }
+
+
